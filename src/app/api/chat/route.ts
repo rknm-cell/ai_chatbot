18ebcb976a804442dbc8464dc7274cfecdea9391
rknm-cell <racknahm@gmail.com>
2025-06-10 18:59:21 -0400

    created route handler for chat

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,94 +1,48 @@
 import { openai } from "@ai-sdk/openai";
 import {
-  appendClientMessage,
   appendResponseMessages,
-  
-  createDataStream,
-  generateId,
+  createIdGenerator,
   streamText,
+  appendClientMessage,
 } from "ai";
-import { getMessagesByChatId } from "~/lib/db/queries";
-import { loadChat, saveChat } from "~/tools/chat-store";
-import { createResumableStreamContext } from 'resumable-stream';
-import { appendStreamId, loadStreams } from "@/util/chat-store";
+import { loadChat } from "~/tools/chat-store";
+import { getChatById, saveChat} from "~/lib/db/queries";
+/* eslint-disable */
+export async function POST(req: Request) {
+  // get the last message from the client:
+  const { message, id, userId, title } = await req.json();
 
+  // load the previous messages from the server:
+  const previousMessages = await getChatById(id);
 
-
-export const maxDuration = 30;
-
-const streamContext = createResumableStreamContext({
-  waitUntil: after,
-})
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const chatId = searchParams.get("chatId");
-
-  if (!chatId) {
-    return new Response("id is required", { status: 400 });
-  }
-  const streamIds = await loadStreams(chatId);
-
-  if (!streamIds.length) {
-    return new Response("No streams found", { status: 404 });
-  }
-  const recentStreamId = streamIds.at(-1);
-
-  if (!recentStreamId) {
-    return new Response("No recent stream found", { status: 404 });
-  }
-  
-  const emptyDataStream = createDataStream({
-    execute: () => {},
+  // append the new message to the previous messages:
+  const messages = appendClientMessage({
+    messages: previousMessages,
+    message,
   });
 
-  const stream = await streamContext.resumableStream(
-    recentStreamId,
-    () => emptyDataStream,
-  );
-  if (stream) {
-    return new Response(stream, { status: 200 });
-  }
 
-  const messages = await getMessagesByChatId({ id: chatId });
-  const mostRecentMessage = messages?.at(-1);
-
-  if (!mostRecentMessage || mostRecentMessage.role !== "assistant") {
-    return new Response(emptyDataStream, { status: 200 });
-  }
-}
-
-export async function POST(req: Request) {
-  // gets the last message from the client:
-  const { messages, id } = await req.json();
-  const streamId = generateId();
-
-  await appendStreamId({ chatId: id, streamId });
-  
-
-  const stream = createDataStream({
-    execute: (dataStream) => {
-      const result = streamText({
-        model: openai("gpt-4o"),
-        messages,
-        onFinish: async ({ response }) => {
-          await saveChat({
-            id,
-            messages: appendResponseMessages({
-              messages,
-              responseMessages: response.messages,
-            }),
-          });
-        },
+  const result = streamText({
+    model: openai("gpt-4o-mini"),
+    messages,
+    experimental_generateMessageId: createIdGenerator({
+      prefix: "msgs",
+      size: 16,
+    }),
+    async onFinish({ response }) {
+      await saveChat({
+        id,
+        title,
+        userId
       });
-      result.mergeIntoDataStream(dataStream);
+
+      
     },
   });
-  return new Response(
-    await streamContext.resumableStream(streamId, () => stream),
-  )
-}
-function after(promise: Promise<unknown>): void {
-  throw new Error("Function not implemented.");
-}
 
+  // consume the stream to ensure it runs to completion & triggers onFinish
+  // even when the client response is aborted:
+  result.consumeStream(); // no await
+
+  return result.toDataStreamResponse();
+}
--- src/lib/db/queries.ts
+++ src/lib/db/queries.ts
@@ -1,9 +1,12 @@
 import "server-only";
 import { asc, eq } from "drizzle-orm";
-import { chat, message, type DBMessage, type Chat } from "./schema";
+import { chat, message, type DBMessage, type Chat, streamIds } from "./schema";
 
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import type { UUID } from "crypto";
+import { generateId } from "ai";
+import { writeFile } from "fs";
 
 const client = postgres(process.env.POSTGRES_URL!);
 const db = drizzle(client);
@@ -23,19 +26,16 @@ export async function saveChat({
   id,
   userId,
   title,
-  visibility,
 }: {
-  id: string;
+  id: UUID;
   userId: string;
   title: string;
-  visibility: string;
 }) {
   return await db.insert(chat).values({
     id,
     createdAt: new Date(),
     userId,
     title,
-    visibility,
   });
 }
 
@@ -45,22 +45,58 @@ export async function getChatById({ id }: { id: string }) {
 }
 
 export async function saveMessages({
-  messages,
+  id,
+  chatId,
+  content,
+  createdAt,
 }: {
-  messages: Array<DBMessage>;
+  id: UUID;
+  chatId: UUID;
+  content: string;
+  createdAt: Date;
 }) {
-  return await db.insert(message).values(messages);
+  return await db.insert(message).values({
+    id,
+    chatId,
+    content,
+    createdAt
+  });
 }
 
 export async function getMessagesByChatId({ id }: { id: string }) {
-  try {
-    return await db
+  // try {
+  //   return await db
+  //     .select()
+  //     .from(message)
+  //     .where(eq(message.chatId, id))
+  //     .orderBy(asc(message.createdAt));
+  // } catch (error) {
+  //   console.error("Failed to get messages by id", error);
+  // }
+  
+  console.log("before const message:", id)
+  const messages = await db
       .select()
       .from(message)
       .where(eq(message.chatId, id))
       .orderBy(asc(message.createdAt));
-  } catch (error) {
-    console.error("Failed to get messages by id", error);
-  }
+      
+  console.log("const message", messages)
+    if (!messages.length){
+      return []
+    } 
+    return messages
 }
 
+
+// export async function loadStreams(chatId: string){
+//   try{
+//     const streams = await db.select().from(streamIds).where(eq(streamIds.chatId, chatId)).orderBy(asc(streamIds.createdAt))
+//     return streams.map(asc(streamIds.createdAt))
+//   }
+// }
+
+// export async function appendStreamId({
+//   chatId: string,
+//   streamId: string,
+// })
