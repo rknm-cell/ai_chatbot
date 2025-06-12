c6a8c3b71fe944bf5cf6ed2fa9204e4ff2b2bf95
rknm-cell <racknahm@gmail.com>
2025-06-12 18:12:28 -0400

    added shadcn and started betterauth

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -2,25 +2,17 @@ import type { InferSelectModel } from "drizzle-orm";
 import {
   pgTable,
   timestamp,
-  primaryKey,
-  uuid,
   text,
   varchar,
-  json,
+  boolean,
 } from "drizzle-orm/pg-core";
 
-export const user = pgTable('User', {
-  id: varchar('id').primaryKey().notNull(),
-  email: varchar('email', { length: 64 }).notNull(),
-  password: varchar('password', { length: 64}),
-});
 export type User = InferSelectModel<typeof user>;
 
-
-export const chat = pgTable('Chat', {
-  id: varchar('id').primaryKey().notNull(),
-  createdAt: timestamp('createdAt').notNull(),
-  title: text('title').notNull(),
+export const chat = pgTable("Chat", {
+  id: varchar("id").primaryKey().notNull(),
+  createdAt: timestamp("createdAt").notNull(),
+  title: text("title").notNull(),
   // userId: uuid('userId')
   //   .notNull()
   //   .references(() => user.id),
@@ -28,15 +20,74 @@ export const chat = pgTable('Chat', {
 
 export type Chat = InferSelectModel<typeof chat>;
 
-export const message = pgTable('Message_v2', {
-  id: varchar('id').primaryKey().notNull(),
-  chatId: varchar('chatId')
+export const message = pgTable("Message_v2", {
+  id: varchar("id").primaryKey().notNull(),
+  chatId: varchar("chatId")
     .notNull()
     .references(() => chat.id),
-  role: varchar('role').notNull(),
-  content: varchar('content'),
-  createdAt: timestamp('createdAt').notNull(),
+  role: varchar("role").notNull(),
+  content: varchar("content"),
+  createdAt: timestamp("createdAt").notNull(),
 });
 
 export type DBMessage = InferSelectModel<typeof message>;
 
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified")
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text("image"),
+  createdAt: timestamp("created_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
+
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  expiresAt: timestamp("expires_at").notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const account = pgTable("account", {
+  id: text("id").primaryKey(),
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  scope: text("scope"),
+  password: text("password"),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+});
+
+export const verification = pgTable("verification", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+  updatedAt: timestamp("updated_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+});
---
+++ src/server/users.ts
@@ -0,0 +1,19 @@
+"use server";
+import { auth } from "@/lib/auth"
+ 
+export const signIn = async () => {
+    await auth.api.signInEmail({
+        body: {
+            email: "user@email.com",
+            password: "password",
+        }
+    })
+}
+export const signUp = async () => {
+    await auth.api.signInEmail({
+        body: {
+            email: "user@email.com",
+            password: "password",
+        }
+    })
+}
