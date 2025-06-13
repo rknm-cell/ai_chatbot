7a6e57d5636c403f38a5bba781433695e3b39c49
rknm-cell <racknahm@gmail.com>
2025-06-13 13:32:37 -0400

    added eslint disable for chat route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { openai } from '@ai-sdk/openai';
 import { streamText } from 'ai';
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
-
+  const { messages } = await req.json()
+  
   const result = streamText({
     model: openai('gpt-4o'),
     messages,
